ef3137ee14dcbfd335ffd44079723f820ce27caf
Huaijie Wang <jyuuku@outlook.com>
2022-05-08 18:59:11 +0800

    test: 加入效率的测试

--- tests/prime.py
+++ tests/prime.py
@@ -1,5 +1,5 @@
 import unittest
-from prime import pwr, gen_g0
+from prime import gen_prime, pwr, gen_g0
 
 
 class primeTest(unittest.TestCase):
@@ -8,6 +8,13 @@ class primeTest(unittest.TestCase):
             g, q = gen_g0(10000)
             for i in range(1, q - 1):
                 assert pwr(g, i, q) != 1
+
+    def test_efficiency(self):
+        import time
+
+        t1 = time.time()
+        print(gen_prime(2**256))
+        print('elapsed: {}'.format(time.time() - t1))
 
 
 if __name__ == '__main__':
